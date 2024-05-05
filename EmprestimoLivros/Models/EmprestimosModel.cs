ce41f51b7228f3267c1b7fa690784601bf74c845
Jair <jair.rollemberg@gmail.com>
2024-05-05 01:26:17 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome de quem vai receber o livro!")]
+        public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do quem vai fornecer o livro!")]
+        public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do livro!")]
+        public string LivroEmprestado { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
